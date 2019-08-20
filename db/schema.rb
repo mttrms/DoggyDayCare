fc23b3655a1a1c9b99b4c59a48698c11f8ca3f56
Matt Ramos <mattramos@gmail.com>
2019-08-20 09:05:48 -0400

    Add Index to dog name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_121702) do
+ActiveRecord::Schema.define(version: 2019_08_20_123837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_121702) do
     t.string "name", null: false
     t.bigint "house_id", null: false
     t.index ["house_id"], name: "index_dogs_on_house_id"
+    t.index ["name"], name: "index_dogs_on_name"
   end
 
   create_table "houses", force: :cascade do |t|
